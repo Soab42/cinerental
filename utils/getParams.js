86c5c3d03d56680f42da453e9f2f2809d2b1a952
Soab42 <108150913+Soab42@users.noreply.github.com>
2024-04-05 00:53:55 +0600

    fix asyc function add async logic

--- utils/getParams.js
+++ utils/getParams.js
@@ -2,11 +2,15 @@ export async function getParams(headers) {
   try {
     const headersList = headers();
     const params = headersList.get("x-params");
+    const { lang, id } = JSON.parse(params);
     if (!params) {
       throw new Error("x-params header not found");
     }
-    const { lang, id } = JSON.parse(params);
-    return { lang, id };
+    return new Promise((resolve) =>
+      setTimeout(() => {
+        resolve({ lang, id });
+      }, 1000)
+    );
   } catch (error) {
     console.error("Error parsing x-params header:", error.message);
     return { lang: null, id: null }; // Return default values or handle the error accordingly
